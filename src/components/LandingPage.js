eac76ef57d61afc70da663117ea5e231c9000a37
Peyton Doyle <p6doyle@gmail.com>
2019-10-17 17:40:00 -0400

    Got something on the page

---
+++ src/components/LandingPage.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class LandingPage extends React.Component {
+  //your code here
+
+  render(){
+  	return (
+  	  <div>
+      <br></br>
+      <h1>All Bots</h1>
+  		</div>
+  	);
+  }
+
+};
+
+export default LandingPage;
