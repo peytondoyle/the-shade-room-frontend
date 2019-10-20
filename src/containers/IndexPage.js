f9bd88bedc6f9cb431c6a2948b0314a517d774d7
Peyton Doyle <p6doyle@gmail.com>
2019-10-20 10:30:30 -0400

    working on navbar, index page navigation, hover effects etc.

--- src/containers/IndexPage.js
+++ src/containers/IndexPage.js
@@ -6,9 +6,72 @@ class IndexPage extends React.Component {
   render(){
   	return (
       <div>
-      <span class="index-header">
-          All Drag Queens
-      </span>
+
+      <div class="row">
+        <div class="col" id="indexpgname">
+          <span class="index-header">
+              All Drag Queens
+          </span>
+        </div>
+        <div class="col col-lg-4" id="indexbuttongroup">
+
+        </div>
+        <div class="col-md-auto" id="indexbuttongroup">
+          <button type="button" class="btn btn-primary"
+          id="indexbuttons">View Random Queen</button>
+        </div>
+
+        <div class="col col-lg-1" id="seasonfilter">
+          <div class="dropdown">
+            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Filter by Season
+            </button>
+              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="#">Season 1</a>
+                <a class="dropdown-item" href="#">Season 2</a>
+                <a class="dropdown-item" href="#">Season 3</a>
+                <a class="dropdown-item" href="#">Season 4</a>
+                <a class="dropdown-item" href="#">Season 5</a>
+                <a class="dropdown-item" href="#">Season 6</a>
+                <a class="dropdown-item" href="#">Season 7</a>
+                <a class="dropdown-item" href="#">Season 8</a>
+                <a class="dropdown-item" href="#">Season 9</a>
+                <a class="dropdown-item" href="#">Season 10</a>
+                <a class="dropdown-item" href="#">Season 11</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col col-lg-1" id="traitfilter">
+            <div class="dropdown">
+              <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Filter by Trait
+              </button>
+              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col col-lg-1" id="sortfilter">
+            <div class="dropdown">
+              <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Sort by
+              </button>
+              <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item" href="#">Action</a>
+                <a class="dropdown-item" href="#">Another action</a>
+                <a class="dropdown-item" href="#">Something else here</a>
+              </div>
+            </div>
+          </div>
+
+      </div>
+
+
+
 
       <div class="row">
         {this.props.allQueens.map(queen => <QueenCard
