631c153fd015887f9ef8ec4ef6d8aba7b22d5362
Peyton Doyle <p6doyle@gmail.com>
2019-10-21 10:04:50 -0400

    Show page work

--- src/components/ShowPage.js
+++ src/components/ShowPage.js
@@ -1,36 +1,58 @@
 import React from "react";
 
 class ShowPage extends React.Component {
-  //your code here
+  Capitalize(str){
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
 
   render(){
   	return (
-  	  <div>
       <div class="container">
-        <br></br>
-        <div class="column">
-          <div class="col-md-4">
-            <h1>{this.props.selectedQueen.name}</h1>
-            <h3>Season {this.props.selectedQueen.season}</h3>
-            <img src={this.props.selectedQueen.image} class="showpage"></img>
-          </div>
+  <div class="row">
+    <div class="col" id="black">
+    <h1>{this.props.selectedQueen.name}</h1>
+    <h3>Season {this.props.selectedQueen.season}</h3>
+    <p>"{this.props.selectedQueen.quote}"</p>
+    <img src={this.props.selectedQueen.image} class="showpage"></img>
+    </div>
+    <div class="col" id="black">
+      <h2>Rate {this.props.selectedQueen.name}</h2>
+    </div>
+    <div class="col" id="black">
+      2 of 2
+    </div>
+  </div><br></br>
+  <div class="row">
+    <div class="col" id="stats">
+      <h2>Winner?</h2>
+        <p>
+          {(() => {
+            switch (this.props.selectedQueen.winner) {
+              case false: return "Nope.";
+              case true: return "Yes!";
+            }
+          })()}
+        </p>
+    </div>
+    <div class="col" id="stats">
+      <h2>Place finished?</h2>
+        {this.Capitalize(this.props.selectedQueen.place)}
+    </div>
+    <div class="col" id="stats">
+      <h2>Miss Congeniality?</h2>
+        <p>
+        {(() => {
+          switch (this.props.selectedQueen.misscongeniality) {
+            case false: return "Nope.";
+            case true: return "Yes!";
+          }
+        })()}
+      </p>
+    </div>
+  </div>
+</div>
 
-          <div class="col-md-auto">
-            <h3>Winner?</h3>
-            <p>lkdshflkajdfglk</p>
-          </div>
-          <div class="col-md-auto">
-            <h3>Placed?</h3>
-            <p>lkdshflkajdfglk</p>
-          </div>
-          <div class="col-md-auto">
-            <h3>Miss Congeniality?</h3>
-            <p>lkdshflkajdfglk</p>
-          </div>
 
-        </div>
-      </div>
-      </div>
   	);
   }
 
---
+++ src/components/showpageTEST.js
@@ -0,0 +1,26 @@
+
+
+<div class="container">
+  <br></br>
+  <div class="column">
+    <div class="col-md-4">
+      <h1>{this.props.selectedQueen.name}</h1>
+      <h3>Season {this.props.selectedQueen.season}</h3>
+      <img src={this.props.selectedQueen.image} class="showpage"></img>
+    </div>
+
+    <div class="col-md-auto">
+      <h3>Winner?</h3>
+      <p>lkdshflkajdfglk</p>
+    </div>
+    <div class="col-md-auto">
+      <h3>Placed?</h3>
+      <p>lkdshflkajdfglk</p>
+    </div>
+    <div class="col-md-auto">
+      <h3>Miss Congeniality?</h3>
+      <p>lkdshflkajdfglk</p>
+    </div>
+
+  </div>
+</div>
